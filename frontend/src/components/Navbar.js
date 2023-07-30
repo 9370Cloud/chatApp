f4794a1eca42ccf47737dcf88e07e55ece160b2b
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-07-30 16:18:16 +0900

    useNavigate

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,31 +1,51 @@
 /* eslint-disable */
 import { Button, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
-function NavBar(){
-    return(
-        <>
-        <Navbar bg="primary" data-bs-theme="dark">
+function NavBar() {
+  let navigate = useNavigate();
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">ChatApp</Navbar.Brand>
+          <Navbar.Brand onClick={(e) => {
+                  e.stopPropagation();
+                  navigate("/");
+                }}>ChatApp</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#pricing">Login</Nav.Link>
+            <Nav.Link >
+              <Button
+                variant="warning"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  navigate("/");
+                }}
+              >
+                Home
+              </Button>{" "}
+            </Nav.Link>
+            <Nav.Link >
+              <Button variant="warning" onClick={(e) => {
+                  e.stopPropagation();
+                  navigate("/login");
+                }}>Login</Button>{" "}
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item >Action</NavDropdown.Item>
+              <NavDropdown.Item >
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item >Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item >
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
-        </>
-    )
+    </>
+  );
 }
 
-export { NavBar }
+export { NavBar };
