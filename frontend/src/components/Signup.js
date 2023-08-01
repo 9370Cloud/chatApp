71f19a3618e75984a9b05285cadd0d96cdb3962e
9370Cloud <alsxmchzhrnrqkq@gmail.com>
2023-08-01 19:23:43 +0900

    아이디 비밀번호 길이 설정

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -9,12 +9,19 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  let navigate = useNavigate();
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
+      // 입력 데이터 길이 검증
+      if (username.length < 6 || username.length > 15) {
+        return setErrorMessage("아이디는 6자 이상 15자 이하로 해조잉");
+      }
+
+      if (password.length < 8 || password.length > 20) {
+        return setErrorMessage("비밀번호는 8자 이상 20자 이하로 해조잉");
+      }
+
       const response = await axios.post("http://localhost:8080/signup", {
         username: username,
         password: password,
@@ -25,16 +32,24 @@ function Signup() {
     } catch (error) {
       // 회원가입 실패하면?
       console.error("실패 ㅋㅋ ", error);
-      if (error.response && error.response.data && error.response.data.message) {
-        setErrorMessage(error.response.data.message); 
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setErrorMessage(error.response.data.message);
       }
     }
   };
 
   return (
-    <Container className="login-wrapper mt-5" style={{ width: '400px', height: '350px' }}>
+    <Container
+      className="login-wrapper mt-5"
+      style={{ width: "400px", height: "350px" }}
+    >
       <h2>Signup</h2>
-      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>} {/* 에러 메시지 출력 */}
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}{" "}
+      {/* 에러 메시지 출력 */}
       <form onSubmit={handleSubmit}>
         <div className="m-2">
           <input
@@ -52,10 +67,12 @@ function Signup() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <Button variant="danger" type="submit">회원가입</Button>
+        <Button variant="danger" type="submit">
+          회원가입
+        </Button>
       </form>
     </Container>
-  )
+  );
 }
 
 export { Signup };
